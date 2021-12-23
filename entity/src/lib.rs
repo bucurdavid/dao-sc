bf69c565a816741550f9ac99c4a3afe7cc1af5c3
micha vie <micha@freeiam.com>
2021-12-23 18:10:23 +0100

    add standard esdt module to entity

--- entity/src/lib.rs
+++ entity/src/lib.rs
@@ -2,10 +2,11 @@
 
 elrond_wasm::imports!();
 
+mod esdt;
 mod features;
 
 #[elrond_wasm::contract]
-pub trait Entity: features::FeaturesModule {
+pub trait Entity: features::FeaturesModule + esdt::EsdtModule {
     #[init]
     fn init(&self) {}
 }
