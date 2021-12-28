8d94253c683e52fede7a81c6307fcd8050b4941d
micha vie <micha@freeiam.com>
2021-12-28 21:03:51 +0100

    add temp setup owners + refactoring

--- manager/src/factory.rs
+++ manager/src/factory.rs
@@ -2,6 +2,10 @@ elrond_wasm::imports!();
 
 #[elrond_wasm::module]
 pub trait FactoryModule {
+    fn init_factory(&self, entity_template_address: ManagedAddress) {
+        self.entity_templ_address().set(&entity_template_address);
+    }
+
     fn create_entity(&self) -> SCResult<ManagedAddress> {
         require!(!self.entity_templ_address().is_empty(), "no template set");
 
@@ -17,8 +21,8 @@ pub trait FactoryModule {
         Ok(address)
     }
 
-    fn create_token_properties(&self, dec_amount: usize) -> FungibleTokenProperties {
-        FungibleTokenProperties {
+    fn issue_token(&self, name: ManagedBuffer, ticker: ManagedBuffer, dec_amount: usize, cost: BigUint) -> AsyncCall {
+        let properties = FungibleTokenProperties {
             num_decimals: dec_amount,
             can_burn: false,
             can_mint: false,
@@ -28,7 +32,28 @@ pub trait FactoryModule {
             can_change_owner: false,
             can_upgrade: false,
             can_add_special_roles: true,
-        }
+        };
+
+        self.send()
+            .esdt_system_sc_proxy()
+            .issue_fungible(cost, &name, &ticker, &BigUint::from(0u32), properties)
+            .async_call()
+    }
+
+    fn set_entity_edst_roles(&self, token_id: &TokenIdentifier, entity_address: &ManagedAddress) -> AsyncCall {
+        let roles = [EsdtLocalRole::Mint, EsdtLocalRole::Burn];
+
+        self.send()
+            .esdt_system_sc_proxy()
+            .set_special_roles(&entity_address, &token_id, (&roles[..]).into_iter().cloned())
+            .async_call()
+    }
+
+    fn transfer_entity_edst_ownership(&self, token_id: &TokenIdentifier, entity_address: &ManagedAddress) -> AsyncCall {
+        self.send()
+            .esdt_system_sc_proxy()
+            .transfer_ownership(&token_id, &entity_address.to_address())
+            .async_call()
     }
 
     #[storage_mapper("entity_templ_addr")]
--- manager/src/lib.rs
+++ manager/src/lib.rs
@@ -8,7 +8,7 @@ mod factory;
 pub trait Manager: factory::FactoryModule {
     #[init]
     fn init(&self, entity_template_address: ManagedAddress) {
-        self.entity_templ_address().set(&entity_template_address);
+        self.init_factory(entity_template_address);
     }
 
     #[payable("EGLD")]
@@ -21,62 +21,49 @@ pub trait Manager: factory::FactoryModule {
         #[payment] issue_cost: BigUint,
     ) -> SCResult<AsyncCall> {
         let initial_caller = self.blockchain().get_caller();
-        let token_properties = self.create_token_properties(dec_amount);
 
         Ok(self
-            .send()
-            .esdt_system_sc_proxy()
-            .issue_fungible(issue_cost, &token_name, &token_ticker, &BigUint::from(0u32), token_properties)
-            .async_call()
+            .issue_token(token_name, token_ticker, dec_amount, issue_cost)
             .with_callback(self.callbacks().token_issue_callback(&initial_caller)))
     }
 
+    #[only_owner]
     #[endpoint(createEntityWithToken)]
     fn create_entity_with_token_endpoint(&self, token: TokenIdentifier) -> SCResult<()> {
-        // 1. ui sets all the right properties
-        // 2. ui transfers ownership
-        require!(token.is_esdt(), "not an esdt");
+        require!(token.is_valid_esdt_identifier(), "not an esdt");
 
-        // check if user is owner and has all properties
+        let caller = self.blockchain().get_caller();
 
-        self.store_new_entity(token);
-
+        self.store_new_entity(&caller, token)?;
         Ok(())
     }
 
-    #[endpoint(setup)]
-    fn setup_endpoint(&self, token_id: TokenIdentifier) -> SCResult<AsyncCall> {
+    #[endpoint(setupEntity)]
+    fn setup_entity_endpoint(&self, token_id: TokenIdentifier) -> SCResult<AsyncCall> {
+        self.require_caller_is_temp_owner(&token_id)?;
         let entity_address = self.get_entity_address(&token_id)?;
-        let roles = [EsdtLocalRole::Mint, EsdtLocalRole::Burn];
 
-        Ok(self
-            .send()
-            .esdt_system_sc_proxy()
-            .set_special_roles(&entity_address, &token_id, (&roles[..]).into_iter().cloned())
-            .async_call())
+        // burn super esdts
+
+        Ok(self.set_entity_edst_roles(&token_id, &entity_address))
     }
 
     #[endpoint(finalize)]
     fn finalize_endpoint(&self, token_id: TokenIdentifier) -> SCResult<AsyncCall> {
+        self.require_caller_is_temp_owner(&token_id)?;
         let entity_address = self.get_entity_address(&token_id)?;
+        let caller = self.blockchain().get_caller();
 
-        Ok(self
-            .send()
-            .esdt_system_sc_proxy()
-            .transfer_ownership(&token_id, &entity_address.to_address())
-            .async_call())
+        self.temp_setup_owner().remove(&caller);
+
+        Ok(self.transfer_entity_edst_ownership(&token_id, &entity_address))
     }
 
     #[callback]
-    fn token_issue_callback(&self, initial_caller: &ManagedAddress, #[call_result] result: ManagedAsyncCallResult<TokenIdentifier>) {
+    fn token_issue_callback(&self, initial_caller: &ManagedAddress, #[call_result] result: ManagedAsyncCallResult<TokenIdentifier>) -> SCResult<()> {
         match result {
-            ManagedAsyncCallResult::Ok(token_id) => self.store_new_entity(token_id),
-            ManagedAsyncCallResult::Err(_) => {
-                let egld_returned = self.call_value().egld_value();
-                if egld_returned > 0u32 {
-                    self.send().direct_egld(&initial_caller, &egld_returned, &[]);
-                }
-            }
+            ManagedAsyncCallResult::Ok(token_id) => self.store_new_entity(initial_caller, token_id),
+            ManagedAsyncCallResult::Err(_) => Ok(self.send_back_egld(&initial_caller)),
         }
     }
 
@@ -90,9 +77,11 @@ pub trait Manager: factory::FactoryModule {
         self.entities_map().get(&token_id).unwrap_or_else(|| ManagedAddress::zero())
     }
 
-    fn store_new_entity(&self, token_id: TokenIdentifier) {
-        let address = self.create_entity().unwrap_or_signal_error();
-        self.entities_map().insert(token_id, address.clone());
+    fn store_new_entity(&self, caller: &ManagedAddress, token_id: TokenIdentifier) -> SCResult<()> {
+        let address = self.create_entity()?;
+        self.entities_map().insert(token_id.clone(), address.clone());
+        self.temp_setup_owner().insert(caller.clone(), token_id);
+        Ok(())
     }
 
     fn get_entity_address(&self, token_id: &TokenIdentifier) -> SCResult<ManagedAddress> {
@@ -100,6 +89,23 @@ pub trait Manager: factory::FactoryModule {
         Ok(self.entities_map().get(&token_id).unwrap())
     }
 
+    fn send_back_egld(&self, initial_caller: &ManagedAddress) {
+        let egld_returned = self.call_value().egld_value();
+        if egld_returned > 0u32 {
+            self.send().direct_egld(&initial_caller, &egld_returned, &[]);
+        }
+    }
+
+    fn require_caller_is_temp_owner(&self, token_id: &TokenIdentifier) -> SCResult<()> {
+        let caller = self.blockchain().get_caller();
+        let temp_owner_token_id = self.temp_setup_owner().get(&caller).unwrap_or(TokenIdentifier::egld());
+        require!(temp_owner_token_id == *token_id, "not in setup");
+        Ok(())
+    }
+
     #[storage_mapper("entities")]
     fn entities_map(&self) -> MapMapper<TokenIdentifier, ManagedAddress>;
+
+    #[storage_mapper("temp_setup_owner")]
+    fn temp_setup_owner(&self) -> MapMapper<ManagedAddress, TokenIdentifier>;
 }
