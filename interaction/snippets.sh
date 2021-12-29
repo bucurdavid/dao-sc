43cee39a6d623db42cad3869ce42962f8306eec4
micha vie <micha@freeiam.com>
2021-12-29 23:22:15 +0100

    update snippets

--- interaction/snippets.sh
+++ interaction/snippets.sh
@@ -11,8 +11,8 @@ ENTITY_DEPLOY_TRANSACTION=$(erdpy data load --partition ${NETWORK_NAME} --key=en
 MANAGER_ADDRESS=$(erdpy data load --partition ${NETWORK_NAME} --key=manager--address)
 MANAGER_DEPLOY_TRANSACTION=$(erdpy data load --partition ${NETWORK_NAME} --key=manager--deploy-transaction)
 
-COST_TOKEN=$(erdpy data load --partition ${NETWORK_NAME} --key=cost-token-id)
-COST_TOKEN_HEX="0x$(echo -n ${COST_TOKEN} | xxd -p -u | tr -d '\n')"
+COST_TOKEN_ID=$(erdpy data load --partition ${NETWORK_NAME} --key=cost-token-id)
+COST_TOKEN_ID_HEX="0x$(echo -n ${COST_TOKEN_ID} | xxd -p -u | tr -d '\n')"
 COST_ENTITY_CREATION_AMOUNT=$(erdpy data load --partition ${NETWORK_NAME} --key=cost-entity-creation-amount)
 
 deploy() {
@@ -41,7 +41,7 @@ deploy() {
 
     echo ">> deploying MANAGER to ${NETWORK_NAME} ..."
     erdpy --verbose contract deploy --project manager \
-        --arguments $entity_template_address_hex ${COST_TOKEN_HEX} ${COST_ENTITY_CREATION_AMOUNT} \
+        --arguments $entity_template_address_hex ${COST_TOKEN_ID_HEX} ${COST_ENTITY_CREATION_AMOUNT} \
         --recall-nonce --gas-limit=80000000 \
         --pem=${DEPLOYER} --outfile="deploy-${NETWORK_NAME}-manager.interaction.json" \
         --proxy=${PROXY} --chain=${CHAIN_ID} \
@@ -52,6 +52,10 @@ deploy() {
 
     erdpy data store --partition ${NETWORK_NAME} --key=manager--address --value=${MANAGER_ADDRESS}
     erdpy data store --partition ${NETWORK_NAME} --key=manager--deploy-transaction --value=${MANAGER_TRANSACTION}
+
+    sleep 10
+
+    addManagerCostTokenBurnRole()
 
     echo ""
     echo "deployed ENTITY smart contract address: ${ENTITY_ADDRESS}"
@@ -66,7 +70,7 @@ upgradeManager() {
 
     echo "upgrading MANAGER contract ${MANAGER_ADDRESS} on ${NETWORK_NAME} ..."
     erdpy --verbose contract upgrade ${MANAGER_ADDRESS} --project manager \
-        --arguments $entity_template_address_hex ${COST_TOKEN_HEX} ${COST_ENTITY_CREATION_AMOUNT} \
+        --arguments $entity_template_address_hex ${COST_TOKEN_ID_HEX} ${COST_ENTITY_CREATION_AMOUNT} \
         --recall-nonce --gas-limit=80000000 \
         --pem=${DEPLOYER} --proxy=${PROXY} --chain=${CHAIN_ID} \
         --send || return
@@ -75,18 +79,55 @@ upgradeManager() {
     echo "upgraded MANAGER smart contract"
 }
 
+upgradeEntityTemplate() {
+    entity_template_address_hex="0x$(erdpy wallet bech32 --decode ${ENTITY_ADDRESS})"
+
+    echo "building contract for upgrade ..."
+    erdpy --verbose contract build entity || return
+
+    echo "upgrading ENTITY template contract ${ENTITY_ADDRESS} on ${NETWORK_NAME} ..."
+    erdpy --verbose contract upgrade ${ENTITY_ADDRESS} --project entity \
+        --arguments ${COST_TOKEN_ID_HEX}\
+        --recall-nonce --gas-limit=80000000 \
+        --pem=${DEPLOYER} --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --send || return
+
+    echo ""
+    echo "upgraded ENTITY template smart contract"
+}
+
+addManagerCostTokenBurnRole() {
+    echo ">> adding ESDTLocalBurn role for ${MANAGER_ADDRESS} ..."
+
+    manager_template_address_hex="0x$(erdpy wallet bech32 --decode ${MANAGER_ADDRESS})"
+    burn_role_hex="0x$(echo -n 'ESDTRoleLocalBurn' | xxd -p -u | tr -d '\n')"
+
+    erdpy --verbose contract call erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u \
+        --function=setSpecialRole \
+        --arguments ${COST_TOKEN_ID_HEX} $manager_template_address_hex $burn_role_hex  \
+        --recall-nonce \
+        --pem=${DEPLOYER} \
+        --gas-limit=60000000 \
+        --proxy=${PROXY} \
+        --chain=${CHAIN_ID} \
+        --send || return
+
+    echo ""
+    echo ">> local burn role added!"
+}
+
 # params:
 #   $1 = token amount
 createEntity() {
-    token_name_hex="0x$(echo -n 'test' | xxd -p -u | tr -d '\n')"
-    token_ticker_hex="0x$(echo -n 'TEST' | xxd -p -u | tr -d '\n')"
+    token_name="0x$(echo -n 'test' | xxd -p -u | tr -d '\n')"
+    token_ticker="0x$(echo -n 'TEST' | xxd -p -u | tr -d '\n')"
 
     erdpy --verbose contract call ${MANAGER_ADDRESS} \
         --recall-nonce \
         --pem=${DEPLOYER} \
         --gas-limit=100000000 \
         --function="createEntity" \
-        --arguments $token_name_hex $token_ticker_hex 0 \
+        --arguments $token_name $token_ticker 0 \
         --value=50000000000000000 \
         --proxy=${PROXY} \
         --chain=${CHAIN_ID} \
@@ -95,25 +136,29 @@ createEntity() {
 
 # params:
 #   $1 = token id
-getEntityAddress() {
-    token_id_hex="0x$(echo -n $1 | xxd -p -u | tr -d '\n')"
+setupEntity() {
+    function="0x$(echo -n 'setupEntity' | xxd -p -u | tr -d '\n')"
+    token_id="0x$(echo -n $1 | xxd -p -u | tr -d '\n')"
 
-    erdpy contract query ${MANAGER_ADDRESS} \
-        --function="getEntityAddress" \
-        --arguments $token_id_hex \
-        --proxy=${PROXY} || return
+    feature1="0x$(echo -n $2 | xxd -p -u | tr -d '\n')"
+    feature2="0x$(echo -n $3 | xxd -p -u | tr -d '\n')"
+
+    erdpy contract call ${MANAGER_ADDRESS} \
+        --function="ESDTTransfer" \
+        --arguments ${COST_TOKEN_ID_HEX} ${COST_ENTITY_CREATION_AMOUNT} $function $token_id $feature1 $feature2 \
+        --pem=${DEPLOYER} \
+        --recall-nonce --gas-limit=80000000 \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --send || return
 }
 
 # params:
 #   $1 = token id
-setup() {
-    token_id_hex="0x$(echo -n $1 | xxd -p -u | tr -d '\n')"
+getEntityAddress() {
+    token_id="0x$(echo -n $1 | xxd -p -u | tr -d '\n')"
 
-    erdpy contract call ${MANAGER_ADDRESS} \
-        --function="setup" \
-        --arguments $token_id_hex \
-        --pem=${DEPLOYER} \
-        --recall-nonce --gas-limit=50000000 \
-        --proxy=${PROXY} --chain=${CHAIN_ID} \
-        --send || return
+    erdpy contract query ${MANAGER_ADDRESS} \
+        --function="getEntityAddress" \
+        --arguments $token_id \
+        --proxy=${PROXY} || return
 }
