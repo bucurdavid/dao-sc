aa583183b71048b77e7883f6aad29b99ce6a2d3c
micha vie <micha@freeiam.com>
2022-03-07 16:46:04 +0100

    update storage views

--- entity/src/governance/storage.rs
+++ entity/src/governance/storage.rs
@@ -10,16 +10,18 @@ pub trait GovStorageModule {
     #[storage_mapper("gov:proposal_start_block")]
     fn proposal_start_block(&self, proposal_id: usize) -> SingleValueMapper<u64>;
 
-    #[storage_mapper("governance:proposal_queue_block")]
+    #[storage_mapper("gov:proposal_queue_block")]
     fn proposal_queue_block(&self, proposal_id: usize) -> SingleValueMapper<u64>;
 
+    #[view(getUpvotes)]
     #[storage_mapper("gov:upvotes")]
     fn upvotes(&self, proposal_id: usize) -> MapMapper<ManagedAddress, BigUint>;
 
+    #[view(getDownvotes)]
     #[storage_mapper("gov:downvotes")]
     fn downvotes(&self, proposal_id: usize) -> MapMapper<ManagedAddress, BigUint>;
 
-    #[view(getTotalVotes)]
+    #[view(getTotalUpvotes)]
     #[storage_mapper("gov:total_upvotes")]
     fn total_upvotes(&self, proposal_id: usize) -> SingleValueMapper<BigUint>;
 
@@ -29,15 +31,15 @@ pub trait GovStorageModule {
 
     // configurable
 
-    #[view(getGovernanceTokenId)]
+    #[view(getGovTokenId)]
     #[storage_mapper("gov:governance_token_id")]
     fn governance_token_id(&self) -> SingleValueMapper<TokenIdentifier>;
 
-    #[view(getQuorum)]
+    #[view(getGovQuorum)]
     #[storage_mapper("gov:quorum")]
     fn quorum(&self) -> SingleValueMapper<BigUint>;
 
-    #[view(getMinTokenBalanceForProposing)]
+    #[view(getMinTokensForProposing)]
     #[storage_mapper("gov:min_token_balance_propose")]
     fn min_token_balance_for_proposing(&self) -> SingleValueMapper<BigUint>;
 
