c7f694d61e72b33a66e5d86ccd7f8d57a2f76aa0
micha vie <micha@freeiam.com>
2022-09-28 14:04:17 +0200

    add min vote weight tests

--- entity/tests/vote_nft_tests.rs
+++ entity/tests/vote_nft_tests.rs
@@ -174,3 +174,38 @@ fn it_fails_to_vote_twice_with_the_same_nft() {
         })
         .assert_user_error("already voted with nft");
 }
+
+#[test]
+fn it_fails_if_less_than_configured_min_vote_weight() {
+    let mut setup = EntitySetup::new(entity::contract_obj);
+    let owner_address = setup.owner_address.clone();
+    let voter_address = setup.user_address.clone();
+    let mut proposal_id = 0;
+
+    setup.configure_gov_token();
+
+    setup.blockchain.set_nft_balance(&owner_address, ENTITY_GOV_TOKEN_ID, 1, &rust_biguint!(1), &0u32);
+    setup.blockchain.set_nft_balance(&voter_address, ENTITY_GOV_TOKEN_ID, 2, &rust_biguint!(1), &0u32);
+
+    setup
+        .blockchain
+        .execute_esdt_transfer(&setup.owner_address, &setup.contract, ENTITY_GOV_TOKEN_ID, 1, &rust_biguint!(1), |sc| {
+            proposal_id = sc.propose_endpoint(
+                managed_buffer!(b"id"),
+                managed_buffer!(b"content hash"),
+                managed_buffer!(b"content signature"),
+                managed_buffer!(b""),
+                MultiValueManagedVec::new(),
+            );
+
+            sc.try_change_min_vote_weight(managed_biguint!(3));
+        })
+        .assert_ok();
+
+    setup
+        .blockchain
+        .execute_esdt_transfer(&voter_address, &setup.contract, ENTITY_GOV_TOKEN_ID, 2, &rust_biguint!(1), |sc| {
+            sc.vote_for_endpoint(proposal_id);
+        })
+        .assert_user_error("not enought vote weight");
+}
