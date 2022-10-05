59b1bd9c9d49ac8da3e4b05bea4997a9ece51637
micha vie <micha@freeiam.com>
2022-10-05 12:08:34 +0200

    add min vote weight tests

---
+++ entity/tests/config_min_vote_weight_tests.rs
@@ -0,0 +1,46 @@
+use elrond_wasm_debug::*;
+use entity::config::*;
+use entity::governance::*;
+use setup::*;
+
+mod setup;
+
+#[test]
+fn it_changes_the_min_proposal_vote_weight_if_contract_calls_itself() {
+    let mut setup = EntitySetup::new(entity::contract_obj);
+
+    setup.configure_gov_token();
+
+    setup
+        .blockchain
+        .execute_tx(setup.contract.address_ref(), &setup.contract, &rust_biguint!(0), |sc| {
+            sc.change_min_vote_weight_endpoint(managed_biguint!(1000));
+
+            assert_eq!(sc.min_vote_weight().get(), managed_biguint!(1000));
+        })
+        .assert_ok();
+}
+
+#[test]
+fn it_fails_if_caller_not_self() {
+    let mut setup = EntitySetup::new(entity::contract_obj);
+
+    setup
+        .blockchain
+        .execute_tx(&setup.owner_address, &setup.contract, &rust_biguint!(0), |sc| {
+            sc.change_min_vote_weight_endpoint(managed_biguint!(1000));
+        })
+        .assert_user_error("action not allowed by user");
+}
+
+#[test]
+fn it_fails_if_gov_token_is_not_set() {
+    let mut setup = EntitySetup::new(entity::contract_obj);
+
+    setup
+        .blockchain
+        .execute_tx(setup.contract.address_ref(), &setup.contract, &rust_biguint!(0), |sc| {
+            sc.change_min_vote_weight_endpoint(managed_biguint!(1000));
+        })
+        .assert_user_error("gov token must be set");
+}
