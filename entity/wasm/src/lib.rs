4d2b8ab45e5493b09a093689e89ac850cff558f9
Micha Vie <micha@freeiam.com>
2023-11-22 16:45:41 +0100

    update build artifacts

--- entity/wasm/src/lib.rs
+++ entity/wasm/src/lib.rs
@@ -5,9 +5,9 @@
 ////////////////////////////////////////////////////
 
 // Init:                                 1
-// Endpoints:                           64
+// Endpoints:                           65
 // Async Callback:                       1
-// Total number of exported functions:  66
+// Total number of exported functions:  67
 
 #![no_std]
 
@@ -75,6 +75,7 @@ multiversx_sc_wasm_adapter::endpoints! {
         sign => sign_endpoint
         execute => execute_endpoint
         directExecute => direct_execute_endpoint
+        cancelProposal => cancel_proposal_endpoint
         withdraw => withdraw_endpoint
         issueGovToken => issue_gov_token_endpoint
         setGovTokenLocalRoles => set_gov_token_local_roles_endpoint
